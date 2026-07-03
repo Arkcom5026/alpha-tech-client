aa1467ccda460619d801fc47c1262542cb926514
Arkcom5026 <advicebanphot@gmail.com>
2026-07-04 01:43:11 +0700

    feat(mission-c): add product template barrel exports

---
+++ src/features/productTemplate/index.js
@@ -0,0 +1,3 @@
+export { default as ProductTemplateGovernanceListPage } from './pages/ProductTemplateGovernanceListPage';
+export { default as ProductTemplateGovernanceDetailPage } from './pages/ProductTemplateGovernanceDetailPage';
+export { default as useProductTemplateStore } from './store/productTemplateStore';

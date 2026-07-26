e3e2c92e53050b643c6e1f63a6fd3a3ebc1fa84f
Arkcom5026 <advicebanphot@gmail.com>
2026-07-26 09:46:16 +0700

    feat(ads): export CRUD action standards

--- src/design-system/composites/index.js
+++ src/design-system/composites/index.js
@@ -1,4 +1,5 @@
 export * from './crud/CrudPage.jsx';
 export * from './crud/CrudToolbar.jsx';
 export * from './crud/CrudPagination.jsx';
+export * from './crud/CrudActions.jsx';
 export * from './dialogs/ConfirmActionDialog.jsx';

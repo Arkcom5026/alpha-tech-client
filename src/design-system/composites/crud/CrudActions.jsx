310dd3dcbbc2d53af7f1830d20efa01de7232eb4
Arkcom5026 <advicebanphot@gmail.com>
2026-07-26 09:46:00 +0700

    feat(ads): add standardized CRUD action components

---
+++ src/design-system/composites/crud/CrudActions.jsx
@@ -0,0 +1,61 @@
+import * as React from 'react';
+import { Button } from '../../foundation.jsx';
+
+const join = (...values) => values.filter(Boolean).join(' ');
+
+const actionVariant = {
+  edit: 'secondary',
+  secondary: 'secondary',
+  destructive: 'danger',
+  restore: 'primary',
+  primary: 'primary',
+};
+
+export const CrudPrimaryAction = React.forwardRef(function CrudPrimaryAction(
+  { className = '', ...props },
+  ref,
+) {
+  return (
+    <Button
+      ref={ref}
+      size="md"
+      variant="primary"
+      className={join('w-full sm:w-auto', className)}
+      {...props}
+    />
+  );
+});
+
+export function CrudTableActions({ align = 'end', className = '', ...props }) {
+  const alignment = {
+    start: 'justify-start',
+    center: 'justify-center',
+    end: 'justify-end',
+  };
+
+  return (
+    <div
+      className={join(
+        'flex flex-wrap items-center gap-2',
+        alignment[align] || alignment.end,
+        className,
+      )}
+      {...props}
+    />
+  );
+}
+
+export const CrudTableAction = React.forwardRef(function CrudTableAction(
+  { action = 'secondary', className = '', ...props },
+  ref,
+) {
+  return (
+    <Button
+      ref={ref}
+      size="sm"
+      variant={actionVariant[action] || actionVariant.secondary}
+      className={join('min-w-20', className)}
+      {...props}
+    />
+  );
+});

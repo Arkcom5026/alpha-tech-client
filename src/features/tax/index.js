805b6db4bcd9238d56038a8818d0712790b24b36
Arkcom5026 <advicebanphot@gmail.com>
2026-07-27 09:53:26 +0700

    feat(tax): expose intake frontend capability

--- src/features/tax/index.js
+++ src/features/tax/index.js
@@ -1,2 +1,4 @@
+export { default as TaxIntakeWorkspacePage } from './intake/pages/TaxIntakeWorkspacePage';
+export * from './intake/api/taxIntakeApi';
 export { default as TaxPeriodManagementPage } from './periods/pages/TaxPeriodManagementPage';
 export * from './periods/api/taxPeriodApi';

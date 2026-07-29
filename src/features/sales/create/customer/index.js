2a1809427725e9b01638f3946c638cf6c245abaa
Arkcom5026 <advicebanphot@gmail.com>
2026-07-29 22:56:54 +0700

    refactor(sales): export customer responsibility shell

--- src/features/sales/create/customer/index.js
+++ src/features/sales/create/customer/index.js
@@ -1,5 +1,7 @@
+export { default as SaleCustomerSection } from './SaleCustomerSection';
 export { default as SaleCustomerSearch } from './components/SaleCustomerSearch';
 export { default as SaleCustomerSearchResults } from './components/SaleCustomerSearchResults';
+export { default as SaleCustomerDetailsForm } from './components/SaleCustomerDetailsForm';
 export { useSaleCustomerSearch } from './hooks/useSaleCustomerSearch';
 export { useSaleCustomerEditor } from './hooks/useSaleCustomerEditor';
 export { useSaleCustomerHydration } from './hooks/useSaleCustomerHydration';

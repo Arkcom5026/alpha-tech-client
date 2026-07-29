b3807f80df711e30f2a81838d0c5ccb1b3643260
Arkcom5026 <advicebanphot@gmail.com>
2026-07-29 23:03:29 +0700

    refactor(sales): expose held cart feature boundary

---
+++ src/features/sales/create/held-cart/index.js
@@ -0,0 +1,7 @@
+export { useSaleHeldCart } from './hooks/useSaleHeldCart';
+export { useSaleHeldCartAutosave } from './hooks/useSaleHeldCartAutosave';
+export {
+  mapHeldCartLinesToSaleItems,
+  projectHeldCartWarning,
+} from './services/saleHeldCartRecovery';
+export { projectSaleHeldCart } from './projections/saleHeldCartProjection';

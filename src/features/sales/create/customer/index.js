2728ad9660808a8f8b5bc04bd11f2f43c013447f
Arkcom5026 <advicebanphot@gmail.com>
2026-07-29 22:48:34 +0700

    refactor(sales): publish customer responsibility boundaries

--- src/features/sales/create/customer/index.js
+++ src/features/sales/create/customer/index.js
@@ -1,2 +1,6 @@
 export { default as SaleCustomerSearch } from './components/SaleCustomerSearch';
 export { default as SaleCustomerSearchResults } from './components/SaleCustomerSearchResults';
+export { useSaleCustomerSearch } from './hooks/useSaleCustomerSearch';
+export { useSaleCustomerEditor } from './hooks/useSaleCustomerEditor';
+export { useSaleCustomerHydration } from './hooks/useSaleCustomerHydration';
+export { projectSaleCustomerSection } from './projections/saleCustomerSectionProjection';

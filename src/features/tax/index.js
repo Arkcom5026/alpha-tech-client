f681bfcc297ef14b6340d85230f7fab7e78c6d24
Arkcom5026 <advicebanphot@gmail.com>
2026-07-29 10:30:54 +0700

    Build input tax control center foundation

--- src/features/tax/index.js
+++ src/features/tax/index.js
@@ -2,3 +2,5 @@ export { default as TaxIntakeWorkspacePage } from './intake/pages/TaxIntakeWorks
 export * from './intake/api/taxIntakeApi';
 export { default as TaxPeriodManagementPage } from './periods/pages/TaxPeriodManagementPage';
 export * from './periods/api/taxPeriodApi';
+export { default as InputTaxControlCenterPage } from './inputTaxControlCenter/pages/InputTaxControlCenterPage';
+export * from './inputTaxControlCenter/api/inputTaxControlCenterApi';

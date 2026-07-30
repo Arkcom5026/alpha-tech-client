231a12e67480d2e2178f3cc31e7fa2bb45b9cd60
Arkcom5026 <advicebanphot@gmail.com>
2026-07-31 03:14:40 +0700

    refactor(stock-item): expose search public boundary

---
+++ src/features/stockItem/search/index.js
@@ -0,0 +1,6 @@
+export { searchStockItemApi } from './api/searchStockItemApi';
+export {
+  projectStockItemSearchError,
+  projectStockItemSearchResult,
+} from './projections/stockItemSearchProjection';
+export { searchStockItem } from './services/searchStockItem';

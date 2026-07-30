1b8a84f5dee0c0a3d2b003bdccec52a967136119
Arkcom5026 <advicebanphot@gmail.com>
2026-07-31 03:30:04 +0700

    refactor(stock-item): expose sold lifecycle public boundary

---
+++ src/features/stockItem/sold/index.js
@@ -0,0 +1,6 @@
+export { markStockItemsAsSoldApi } from './api/markStockItemsAsSoldApi';
+export {
+  normalizeStockItemIdsForSold,
+  projectStockItemSoldError,
+} from './projections/stockItemSoldProjection';
+export { markStockItemsAsSold } from './services/markStockItemsAsSold';

8b3fbe01aa62672211a84cf92835006d7f38da91
Arkcom5026 <advicebanphot@gmail.com>
2026-07-30 21:52:35 +0700

    feat(barcode): export receipt finalization slice

---
+++ src/features/barcode/finalization/index.js
@@ -0,0 +1,6 @@
+export { finalizeReceiptApi } from './api/finalizeReceiptApi';
+export {
+  projectReceiptFinalizationCommand,
+  projectReceiptFinalizationResult,
+} from './projections/receiptFinalizationProjection';
+export { finalizeReceipt } from './services/finalizeReceipt';

606bc64d1b3630116e804bc90283ecae6cd079dc
Arkcom5026 <advicebanphot@gmail.com>
2026-07-30 19:48:39 +0700

    feat(barcode): export scan serial slice

---
+++ src/features/barcode/scan-serial/index.js
@@ -0,0 +1,17 @@
+export {
+  receiveScannedStockItemApi,
+  updateBarcodeSerialNumberApi,
+  commitReceiptScansApi,
+} from './api/barcodeScanApi';
+export {
+  projectScanInput,
+  projectReceivePayload,
+  projectCommitScanItems,
+  projectCommitScanResult,
+  projectBarcodeScanError,
+} from './projections/barcodeScanProjection';
+export {
+  receiveScannedStockItem,
+  assignBarcodeSerialNumber,
+  commitReceiptScans,
+} from './services/barcodeScanService';

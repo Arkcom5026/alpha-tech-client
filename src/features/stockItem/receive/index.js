feb222dc4b0651f51be89a59e83888fd03b7d4d7
Arkcom5026 <advicebanphot@gmail.com>
2026-07-31 03:07:13 +0700

    refactor(stock-item): publish complete receive slice boundary

--- src/features/stockItem/receive/index.js
+++ src/features/stockItem/receive/index.js
@@ -1,6 +1,8 @@
 export { receiveStockItemApi } from './api/receiveStockItemApi';
+export { receiveAllPendingStockItemsApi } from './api/receiveAllPendingStockItemsApi';
 export {
   projectStockItemReceiveCommand,
   projectStockItemReceiveResult,
 } from './projections/stockItemReceiveProjection';
 export { receiveScannedStockItem } from './services/receiveScannedStockItem';
+export { receiveAllPendingStockItems } from './services/receiveAllPendingStockItems';

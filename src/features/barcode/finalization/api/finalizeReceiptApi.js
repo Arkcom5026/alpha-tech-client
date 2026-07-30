51d8e0702d7051bdca98ea12a27f917f0d64a623
Arkcom5026 <advicebanphot@gmail.com>
2026-07-31 00:05:19 +0700

    refactor: retire barcode-owned receipt finalization api

--- src/features/barcode/finalization/api/finalizeReceiptApi.js
+++
@@ -1,9 +0,0 @@
-import apiClient from '@/utils/apiClient';
-
-export const finalizeReceiptApi = async ({ receiptId }) => {
-  const response = await apiClient.patch(
-    `/purchase-order-receipts/${receiptId}/finalize`,
-  );
-
-  return response?.data;
-};

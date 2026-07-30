a03cfc210396cb7706ee2d40b81a8e3fc762a37c
Arkcom5026 <advicebanphot@gmail.com>
2026-07-30 22:50:25 +0700

    refactor(barcode): remove StockItem receive API ownership

--- src/features/barcode/scan/api/receiveStockItemApi.js
+++
@@ -1,6 +0,0 @@
-import apiClient from '@/utils/apiClient';
-
-export const receiveStockItemApi = async (payload) => {
-  const response = await apiClient.post('/stock-items/receive-sn', payload);
-  return response.data;
-};

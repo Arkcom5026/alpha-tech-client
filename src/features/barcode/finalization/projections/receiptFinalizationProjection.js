89ab7d4a60a2d6dd408d1e4ad07fdce2e73118c0
Arkcom5026 <advicebanphot@gmail.com>
2026-07-31 00:05:37 +0700

    refactor: retire barcode-owned receipt finalization projection

--- src/features/barcode/finalization/projections/receiptFinalizationProjection.js
+++
@@ -1,9 +0,0 @@
-export const projectReceiptFinalizationCommand = (receiptId) => {
-  if (!receiptId) throw new Error('Missing receiptId');
-
-  return { receiptId };
-};
-
-export const projectReceiptFinalizationResult = (sourceResponse) => ({
-  sourceResponse,
-});

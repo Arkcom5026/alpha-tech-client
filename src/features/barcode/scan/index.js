6ec6bfb77e68814bf750b1126613214aaafa2850
Arkcom5026 <advicebanphot@gmail.com>
2026-07-30 22:50:53 +0700

    refactor(barcode): retire StockItem receive scan slice

--- src/features/barcode/scan/index.js
+++
@@ -1,6 +0,0 @@
-export { receiveStockItemApi } from './api/receiveStockItemApi';
-export {
-  projectStockItemReceiveCommand,
-  projectStockItemReceiveResult,
-} from './projections/stockItemReceiveProjection';
-export { receiveScannedStockItem } from './services/receiveScannedStockItem';

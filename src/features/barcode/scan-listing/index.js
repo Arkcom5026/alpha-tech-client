79c7fad7b89bef2778642aade16a37ab68e14792
Arkcom5026 <advicebanphot@gmail.com>
2026-07-30 20:37:08 +0700

    feat(barcode): expose scan listing foundation

---
+++ src/features/barcode/scan-listing/index.js
@@ -0,0 +1,9 @@
+export {
+  getReceiptsReadyToScanApi,
+  getReceiptsReadyToScanSnApi,
+} from './api/getReceiptsReadyToScanApi';
+export { projectScanReceiptListingResult } from './projections/scanReceiptListingProjection';
+export {
+  listReceiptsReadyToScan,
+  listReceiptsReadyToScanSn,
+} from './services/listReceiptsReadyToScan';

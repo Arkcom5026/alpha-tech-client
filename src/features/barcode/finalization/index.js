16e34e40d1686cdb6ba239259b0da8019104a713
Arkcom5026 <advicebanphot@gmail.com>
2026-07-31 00:06:16 +0700

    refactor: retire barcode receipt finalization boundary

--- src/features/barcode/finalization/index.js
+++
@@ -1,6 +0,0 @@
-export { finalizeReceiptApi } from './api/finalizeReceiptApi';
-export {
-  projectReceiptFinalizationCommand,
-  projectReceiptFinalizationResult,
-} from './projections/receiptFinalizationProjection';
-export { finalizeReceipt } from './services/finalizeReceipt';

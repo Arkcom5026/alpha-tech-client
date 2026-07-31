e52084f8767cca705a05af145f3cd32bed3bd604
Arkcom5026 <advicebanphot@gmail.com>
2026-07-31 20:40:03 +0700

    refactor(receiving): retire quick stock toolbar wrapper

--- src/features/receiving/quick-stock/components/QuickStockToolbar.jsx
+++
@@ -1,5 +0,0 @@
-// src/features/receiving/quick-stock/components/QuickStockToolbar.jsx
-
-import IntakeControlPanel from "../../components/quick-stock/IntakeControlPanel";
-
-export default IntakeControlPanel;

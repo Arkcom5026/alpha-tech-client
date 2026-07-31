15275cccaaf2189976e701c3ebb1cada7e85030e
Arkcom5026 <advicebanphot@gmail.com>
2026-07-31 20:35:24 +0700

    refactor(receiving): retire quick stock finder wrapper

--- src/features/receiving/quick-stock/components/QuickStockFinderPanel.jsx
+++
@@ -1,5 +0,0 @@
-// src/features/receiving/quick-stock/components/QuickStockFinderPanel.jsx
-
-import ProductFinderPanel from "../../components/quick-stock/ProductFinderPanel";
-
-export default ProductFinderPanel;

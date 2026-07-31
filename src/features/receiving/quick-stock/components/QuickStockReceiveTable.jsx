0097f976e60da1dbeeee093d29746bb57b7bbb6e
Arkcom5026 <advicebanphot@gmail.com>
2026-07-31 20:37:48 +0700

    refactor(receiving): retire quick stock receive table wrapper

--- src/features/receiving/quick-stock/components/QuickStockReceiveTable.jsx
+++
@@ -1,5 +0,0 @@
-// src/features/receiving/quick-stock/components/QuickStockReceiveTable.jsx
-
-import ProductMasterPanel from "../../components/quick-stock/ProductMasterPanel";
-
-export default ProductMasterPanel;

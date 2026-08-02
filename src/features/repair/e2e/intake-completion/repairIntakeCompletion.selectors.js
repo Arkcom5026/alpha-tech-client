4b190f75bc8041f2bfc184e1e8fb7e5a5f720c8e
Arkcom5026 <advicebanphot@gmail.com>
2026-08-03 00:58:02 +0700

    test(repair-e2e): add stable repair selectors

---
+++ src/features/repair/e2e/intake-completion/repairIntakeCompletion.selectors.js
@@ -0,0 +1,6 @@
+export const repairIntakeSelectors = Object.freeze({
+  loginIdentity: 'input[placeholder="อีเมลหรือเบอร์โทรศัพท์"]',
+  loginPassword: 'input[type="password"]',
+  statusSelect: 'select',
+  evidenceFileInput: 'input[type="file"]',
+});

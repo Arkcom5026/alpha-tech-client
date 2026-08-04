c65759f4bfbb00770c19e24c41bdcf032ce354d5
Arkcom5026 <advicebanphot@gmail.com>
2026-08-04 17:42:42 +0700

    feat(printing): expose print authority foundation

---
+++ src/features/printing/authority/index.js
@@ -0,0 +1,13 @@
+export {
+  PRINT_DOCUMENT_TYPES,
+  PRINT_JOB_STATUSES,
+  createPrintJobContract,
+} from './createPrintJobContract'
+
+export {
+  DEFAULT_LOCAL_PRINT_BRIDGE_URL,
+  LocalPrintBridgeUnavailableError,
+  createLocalPrintBridgeTransport,
+} from './localPrintBridgeTransport'
+
+export { createPrintAuthorityService } from './printAuthorityService'

9da4e939dc9473cf6919040b3b5bea29709c8ef8
Arkcom5026 <advicebanphot@gmail.com>
2026-08-06 23:11:08 +0700

    feat(printing): export printer preference foundation

---
+++ src/features/printing/preferences/index.js
@@ -0,0 +1,10 @@
+export {
+  DOCUMENT_PURPOSES,
+  createPrinterPreferenceContract,
+  createPrinterPreferenceKey,
+} from './createPrinterPreferenceContract.js'
+
+export {
+  STORAGE_NAMESPACE,
+  createPrinterPreferenceStore,
+} from './printerPreferenceStore.js'

e8411ec6d7ad2e0599e3f12fe473fcf58aa7e58e
Arkcom5026 <advicebanphot@gmail.com>
2026-08-06 23:22:26 +0700

    feat(printing): export printer settings UI foundation

---
+++ src/features/printing/settings/index.js
@@ -0,0 +1,6 @@
+export { PrinterSettingsPanel } from './PrinterSettingsPanel.jsx'
+export {
+  PURPOSE_LABELS,
+  createPrinterSettingsRows,
+  describePrinter,
+} from './printerSettingsViewModel.js'

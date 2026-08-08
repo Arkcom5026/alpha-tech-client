2f850ae9ba4daba1123e101abb8cb86c9ec3e74c
Arkcom5026 <advicebanphot@gmail.com>
2026-08-08 14:21:46 +0700

    refactor(branch): expose workspace public boundary

---
+++ src/features/branch/workspace/index.js
@@ -0,0 +1,7 @@
+export { default as BranchListWorkspace } from './BranchListWorkspace';
+export {
+  filterBranchesForShop,
+  isBranchSuperAdmin,
+  projectBranchEditDefaults,
+  resolveBranchSlug,
+} from './branchWorkspacePolicy';

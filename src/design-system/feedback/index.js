005437c049c17bf1f7a01d4b95bc04745e69afc3
Arkcom5026 <advicebanphot@gmail.com>
2026-08-15 08:53:17 +0700

    feat(ui): export feedback foundation

---
+++ src/design-system/feedback/index.js
@@ -0,0 +1,4 @@
+export { default as FeedbackProvider } from './FeedbackProvider.jsx';
+export { feedback } from './feedback.js';
+export { InlineFeedback, FieldMessage, FormErrorSummary } from './InlineFeedback.jsx';
+export { getSafeErrorMessage } from './errorPresentation.js';
